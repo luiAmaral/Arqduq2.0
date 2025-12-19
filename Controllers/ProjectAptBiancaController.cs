68aa81872521b0894a3df1ef88feb90d91c38d91
Luis Amaral <Luis.Amaral@RO.SABENAUTO>
2025-12-19 17:29:56 -0400

    feat: implements first project

---
+++ Controllers/ProjectAptBiancaController.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using Arqduqq.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arqduqq.Controllers
+{
+    public class ProjectAptBiancaController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
