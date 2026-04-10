65fb40ff64633234c5d60dfd8e55e179b6ad4c9f
Bernardo N <bernardoniehues01@gmail.com>
2026-04-10 02:32:20 -0300

    ajuste css pra refletir na azure

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.FileProviders; // Adicione este
+using System.IO;                          // Adicione este
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,15 +12,20 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
+// Configuração para forçar a busca na pasta raiz do Azure
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot")),
+    RequestPath = "" 
+});
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
